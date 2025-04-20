d16933fefbc1cc337ae0cfac834acb4b02525463
TTW97 <68407647+TTW97@users.noreply.github.com>
2025-04-20 11:49:21 -0700

    Add files via upload

---
+++ Session1Prompt.py
@@ -0,0 +1,7 @@
+'''Write a Python program to print out your full name, your preferred pronouns 
+(optional), and two sentences about your favorite movie and your favorite food.
+'''
+
+print('Sneh Shihora')
+print('Preferred Pronouns: He/Him')
+print('My favorite movie would be the entire Lord of the Rings Trilogy & My favorite food would be Samosas (An Indian Dish)')
